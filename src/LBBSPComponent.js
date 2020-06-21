91fdf2fc1a036bf1bdaa178711be220df5cbd9e1
Anwesh43 <anweshthecool0@gmail.com>
2020-06-21 12:30:45 +0530

    adding LBBSPComponent

---
+++ src/LBBSPComponent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useStyle} from './hooks'
+
+const CurrDiv = ({style}) => {
+    return <div style = {style}></div>
+}
+
+const LBBSPComponent = ({w, h, scale, onClick}) => {
+    const {getLineStyle, getCircleStyle} = useStyle(w, h, scale)
+    return (<div>
+        {[0, 1].map(i => <CurrDiv key = `circle_${i}` style = {getCircleStyle(i)}/>)}
+        {[0, 1].map(i => <CurrDiv key = `line_${i}` style = {getLineStyle(i)}/>)}
+    </div>)
+}
+
+export default LBBSPComponent
